89080943eb59020cc93c652349b6199628ac6582
aurels <aurels@gangsta.local>
2009-05-16 11:22:01 +0200

    more unit tests :)

--- test/unit/validates_email_test.rb
+++ test/unit/validates_email_test.rb
@@ -6,9 +6,20 @@ class MiscValidatorsValidatesEmailTest < ActiveSupport::TestCase
   def test_good_email
     assert User.new(:email => "dude.lastname@yahoo.cz").valid?
     assert User.new(:email => "dude@gmail.com").valid?
+    assert User.new(:email => "dude@gmail.co.uk").valid?
+    assert User.new(:email => "Francois.Stephany@gmail.com").valid?
+    assert User.new(:email => "Francois.Stephay@gmail.co.uk").valid?
+    assert User.new(:email => "dude-blip@gmail.com").valid?
+    assert User.new(:email => "dude_blop@gmail.com").valid?
+    assert User.new(:email => "dude789@gmail.com").valid?
   end
   
   def test_wrong_email
     assert !User.new(:email => "thisisnotanemail").valid?
+    assert !User.new(:email => "@thisisnotanemail").valid?
+    assert !User.new(:email => "thisisnotanemail@").valid?
+    assert !User.new(:email => "thisisnotanemail@gmail").valid?
+    assert !User.new(:email => "thisisnotanemail@hotmail@gmail.com").valid?
+    assert !User.new(:email => "this.is.not.anemail").valid?
   end
 end
--- test/unit/validates_url_test.rb
+++ test/unit/validates_url_test.rb
@@ -8,11 +8,20 @@ class MiscValidatorsValidatesUrlTest < ActiveSupport::TestCase
     assert Bookmark.new(:url => "http://google.com").valid?
     assert Bookmark.new(:url => "https://www.google.com").valid?
     assert Bookmark.new(:url => "http://www.google.com/test").valid?
+    assert Bookmark.new(:url => "http://www.google.com/test-blop").valid?
+    assert Bookmark.new(:url => "http://www.google.com/~aurels").valid?
+    assert Bookmark.new(:url => "http://localhost").valid?
+    assert Bookmark.new(:url => "http://www.google.com:890/").valid?
+    assert Bookmark.new(:url => "http://www.google.com:80").valid?
+    assert Bookmark.new(:url => "http://www.google.com/blop:8080").valid?
   end
   
   def test_wrong_url
     assert !Bookmark.new(:url => "www.google.com").valid?
     assert !Bookmark.new(:url => "google").valid?
     assert !Bookmark.new(:url => "ftp://www.google.com").valid?
+    assert !Bookmark.new(:url => "ftp://www.google.com").valid?
+    assert !Bookmark.new(:url => "http://www.google.com:").valid?
+    assert !Bookmark.new(:url => "http://www.goo:gle.com").valid?
   end
 end
